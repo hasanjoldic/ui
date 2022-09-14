eb1b578ec6872e5e6daa486812aacc6324d77fec
Hasan Joldic <hasan.joldic@enki.ba>
2022-09-14 05:42:03 +0000

    refactor(theme): don't use menu with palette mode button

--- packages/theme/src/components/PaletteModeButton.tsx
+++ packages/theme/src/components/PaletteModeButton.tsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
+// import { useState } from "react";
 
-import type { PaletteMode } from "@mui/material";
+// import type { PaletteMode } from "@mui/material";
 
 import IconButton from "@mui/material/IconButton";
-import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
-import ListItemIcon from "@mui/material/ListItemIcon";
+// import Menu from "@mui/material/Menu";
+// import MenuItem from "@mui/material/MenuItem";
+// import ListItemIcon from "@mui/material/ListItemIcon";
 
 import DarkMode from "@mui/icons-material/DarkMode";
 import LightMode from "@mui/icons-material/LightMode";
@@ -16,53 +16,54 @@ import { useTheme } from "../context";
 export const PaletteModeButton: React.FC = () => {
   const { paletteMode, setPaletteMode } = useTheme();
 
-  const [anchorEl, setAnchorEl] = useState<HTMLElement>();
+  // const [anchorEl, setAnchorEl] = useState<HTMLElement>();
 
-  const handleOpen = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
+  // const handleOpen = (event: React.MouseEvent<HTMLButtonElement>) => {
+  //   setAnchorEl(event.currentTarget);
+  // };
 
-  const handleClose = () => {
-    setAnchorEl(undefined);
-  };
+  // const handleClose = () => {
+  //   setAnchorEl(undefined);
+  // };
 
-  const handleChange = (paletteMode: PaletteMode) => () => {
-    setPaletteMode(paletteMode);
+  // const handleChange = (paletteMode: PaletteMode) => () => {
+  //   setPaletteMode(paletteMode);
 
-    handleClose();
-  };
+  //   handleClose();
+  // };
   return (
-    <>
-      <IconButton onClick={handleOpen}>
-        {/* {paletteMode === "system" && <SettingsBrightness />} */}
-        {paletteMode === "light" && <LightMode />}
-        {paletteMode === "dark" && <DarkMode />}
-      </IconButton>
-      <Menu
-        id="basic-menu"
-        anchorEl={anchorEl}
-        open={!!anchorEl}
-        onClose={handleClose}
-      >
-        {/* <MenuItem onClick={handleChange("system")}>
-          <ListItemIcon>
-            <SettingsBrightness />
-          </ListItemIcon>
-          System
-        </MenuItem> */}
-        <MenuItem onClick={handleChange("light")}>
-          <ListItemIcon>
-            <LightMode />
-          </ListItemIcon>
-          Light
-        </MenuItem>
-        <MenuItem onClick={handleChange("dark")}>
-          <ListItemIcon>
-            <DarkMode />
-          </ListItemIcon>
-          Dark
-        </MenuItem>
-      </Menu>
-    </>
+    <IconButton
+      onClick={() => setPaletteMode(paletteMode === "light" ? "dark" : "light")}
+      size="large"
+    >
+      {/* {paletteMode === "system" && <SettingsBrightness />} */}
+      {paletteMode === "light" && <LightMode />}
+      {paletteMode === "dark" && <DarkMode />}
+    </IconButton>
+    // <Menu
+    //   id="basic-menu"
+    //   anchorEl={anchorEl}
+    //   open={!!anchorEl}
+    //   onClose={handleClose}
+    // >
+    //   {/* <MenuItem onClick={handleChange("system")}>
+    //     <ListItemIcon>
+    //       <SettingsBrightness />
+    //     </ListItemIcon>
+    //     System
+    //   </MenuItem> */}
+    //   <MenuItem onClick={handleChange("light")}>
+    //     <ListItemIcon>
+    //       <LightMode />
+    //     </ListItemIcon>
+    //     Light
+    //   </MenuItem>
+    //   <MenuItem onClick={handleChange("dark")}>
+    //     <ListItemIcon>
+    //       <DarkMode />
+    //     </ListItemIcon>
+    //     Dark
+    //   </MenuItem>
+    // </Menu>
   );
 };
