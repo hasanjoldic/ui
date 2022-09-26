d92281112c88c571188d5707e0401b1896dbee9b
Hasan Joldic <office@hasanjoldic.com>
2022-09-26 06:57:36 +0000

    style(components): max width on small screens

--- packages/components/src/page/PageContent.tsx
+++ packages/components/src/page/PageContent.tsx
@@ -4,11 +4,16 @@ import styled from "@mui/material/styles/styled";
 import Box from "@mui/material/Box";
 
 const Container = styled(Box)(({ theme }) => ({
+  maxWidth: "100vw",
   flexGrow: 1,
   padding: theme.spacing(2, 4),
   display: "flex",
   flexDirection: "column",
   alignItems: "center",
+
+  [theme.breakpoints.down("sm")]: {
+    padding: theme.spacing(1, 1),
+  },
 }));
 
 export const PageContent: React.FC<React.PropsWithChildren> = ({
